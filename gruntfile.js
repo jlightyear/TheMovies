ffd09cc5aaf1fdc48266e55c30a48f4f5a5b652e
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-09 17:33:32 +0100

    Twitter section completed

--- gruntfile.js
+++ gruntfile.js
@@ -15,21 +15,6 @@ module.exports = function(grunt) {
 			css: {
 				src: ['css/*.css'],
 				dest: 'dist/<%= pkg.name %>.css'
-			},
-
-			spec: {
-				src: 'spec/*Spec.js',
-				dest: 'dist/spec/spec.js'
-			}
-		},
-
-		jasmine: {
-			pivotal: {
-				src: 'spec/*.js',
-				options: {
-					specs: 'spec/*Spec.js',
-					helpers: 'spec/*Helper.js'
-				}
 			}
 		},
 
@@ -43,7 +28,6 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['concat']);
